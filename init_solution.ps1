e6f19c5f4abb9ee564d6ff54a94b1763a845cabb
Apollo3zehn <git@m1.apollo3zehn.net>
2023-03-22 21:08:31 +0100

    Reenable tests.

--- init_solution.ps1
+++ init_solution.ps1
@@ -9,7 +9,7 @@ Set-Location -Path $path
 
 if ($IsWindows)
 {
-	cmake ./../../native/c-blosc2 -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 16 2019" -A "Win32"  -DBUILD_TESTS=OFF
+	cmake ./../../native/c-blosc2 -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 16 2019" -A "Win32"
 }
 elseif ($IsLinux)
 {
@@ -28,11 +28,11 @@ Set-Location -Path $path
 
 if ($IsWindows)
 {
-	cmake ./../../native/c-blosc2 -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 16 2019" -A "x64" -DBUILD_TESTS=OFF
+	cmake ./../../native/c-blosc2 -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 16 2019" -A "x64"
 }
 elseif ($IsLinux)
 {
-    cmake ./../../native/c-blosc2 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DBUILD_TESTS=OFF
+    cmake ./../../native/c-blosc2 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64
 }
 else 
 {
